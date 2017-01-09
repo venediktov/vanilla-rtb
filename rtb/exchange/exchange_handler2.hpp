cb45ecd5160cdbbbb6c5cd41c74500bef35d0a71
vvenedik <vvenedik@NB-17-1088>
2017-01-09 12:02:07 -0800

    work in progress

---
+++ rtb/exchange/exchange_handler2.hpp
@@ -0,0 +1,75 @@
+/*
+ * File:   exchange_handler.hpp
+ * Author: Vladimir Venediktov
+ * Copyright (c) 2016-2018 Venediktes Gruppe, LLC
+ *
+ * Created on December 25, 2016, 12:40 PM
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*
+*/
+
+#pragma once
+
+#include <string>
+#include <functional>
+#include "CRUD/service/reply.hpp"
+#include "CRUD/handlers/crud_matcher.hpp"
+
+namespace vanilla { namespace exchange {
+
+
+template<typename  DSL>
+class exchange_handler {
+
+using auction_request_type = decltype(DSL().extract_request(std::string()));
+using auction_response_type = typename DSL::serialized_type;
+using auction_handler_type = std::function<auction_response_type (const auction_request_type &,const std::chrono::milliseconds &)>; 
+template<typename Match>
+using log_handler_type = std::function<void (const http::crud::crud_match<Match> &)>;
+using self_type = exchange_handler<DSL> ;
+
+DSL parser;
+auction_handler_type auction_handler;
+log_handler_type log_handler;
+
+public:
+    exchange_handler() : parser{}, auction_handler{}, log_handler{} 
+    {}
+    self_type & auction(const auction_handler_type &handler) {
+        auction_handler = handler;
+        return *this;
+    }
+
+    template<typename Match>
+    self_type & logger(const log_handler_type<Match> &handler) {
+        log_handler = handler;
+        return *this;
+    }
+
+    template<typename Match>
+    handle_post(http::server::reply & r, const http::crud::crud_match<Match>  & match) {
+        if ( log_handler ) {
+            log_handler(match);
+        }
+        auto bid_request = parser.extract_request(match.data) ;
+        if ( auction_handler ) {
+            std::chrono::milliseconds timeout{bid_request.tmax};
+            auto auction_response = auction_handler(bid_request,timeout) ;
+            auto wire_response = parser.create_response(auction_response) ;
+            r << to_string(wire_response) << http::server::reply::flush("");
+        }
+    }
+    
+};
+
+}}
+
---
+++ rtb/exchange/exchange_server.hpp
@@ -0,0 +1,78 @@
+/*
+ * File:   exchange_server.hpp
+ * Author: Vladimir Venediktov
+ * Copyright (c) 2016-2018 Venediktes Gruppe, LLC
+ *
+ * Created on January 09, 2016, 12:40 PM
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*
+*/
+
+#pragma once
+
+#include <string>
+#include <thread>
+#include <tuple>
+#include <functional>
+#include <boost/asio.hpp>
+#include "CRUD/service/server.hpp"
+
+namespace vanilla { namespace exchange {
+
+struct connection_endpoint {
+    using ctor_type = std::tuple<std::string, std::string, std::string>;
+    std::string host;
+    std::string port;
+    std::string root;
+    connection_endpoint(const ctor_type &ep) : host{ std::get<0>(ep) }, port{ std::get<1>(ep) }, root{ std::get<2>(ep) }
+    {}
+   
+};
+
+//allows to implement custom dispatcher 
+template<typename RestfulDispatcherT>
+struct exchange_server {
+
+connection_endpoint ep;
+RestfulDispatcherT dispatcher;
+unsigned int hardware_threads;
+
+public:
+    //make it non-copyable non-movable
+    exchange_server(const exchange_server&) = delete;
+    exchange_server& operator=(const exchange_server&) = delete;
+    exchange_server(exchange_server&&) = delete;
+    exchange_server& operator=(exchange_server&&) = delete;
+
+    exchange_server(const connection_endpoint &ep, const RestfulDispatcherT &dispatcher) : 
+        ep{ep}, dispatcher{dispatcher}, hardware_threads{ std::max(1u, std::thread::hardware_concurrency()) }
+    {}
+
+    void run() {
+       http::server::server<RestfulDispatcherT> server{ep.host,ep.port,dispatcher} ;
+       std::vector<std::shared_ptr<std::thread>> threads;
+       for ( unsigned int i=0; i < hardware_threads ; ++i) {
+            threads.push_back( std::make_shared<std::thread>( [&server] () {
+                server.run() ;
+            }));
+       }
+
+       for ( auto thread : threads ) {
+           thread->join();
+       }
+    
+    }
+
+};
+
+}}
+
