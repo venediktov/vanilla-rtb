940e0b6cdafd19d4979d3c3e1564741c5782385a
abushev <arseny.bushev@gmail.com>
2017-04-18 01:24:20 +0300

    core.hpp

--- rtb/core/core.hpp
+++ rtb/core/core.hpp
@@ -12,18 +12,7 @@
 #include <boost/log/trivial.hpp>
 #include <boost/log/expressions.hpp>
 
-/*
-#ifdef NDEBUG
-#include <iosfwd>
-struct nullstream_t : public std::ostream {
-    nullstream_t() : std::ios(0), std::ostream(0) {}
-};
-
-static nullstream_t nullstream;
-#define LOG(x) nullstream
-#else */
 #define LOG(x) BOOST_LOG_TRIVIAL(x)
-//#endif
 
 #endif /* CORE_HPP */
 
