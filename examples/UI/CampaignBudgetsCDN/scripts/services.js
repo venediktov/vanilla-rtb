6eec15bf2e6adf03e1c9d86edd2456e23b45d6c1
Serge <urakhchin@gmail.com>
2017-05-28 11:34:42 -0700

    Removed /bower_components, CampaignBudgetsDev and CampaignBudgetsDist and replaced with CampaignBudgetsCDN. I manually went into index.html and one by one replaced each dependency (making sure to match the versions) with a CDN from the internet.
    I also removed any related mentions in the code
    
    Also changed the baseURL to http://localhost:11081/campaign/ and have the angular resource call the budget/:id object.
    
    I think it should be able to work with your server.... SHOULD lol

---
+++ examples/UI/CampaignBudgetsCDN/scripts/services.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('budgetsApp')
+
+
+        .constant("baseURL","http://localhost:11081/campaign/")
+        .service('budgetFactory', ['$resource', 'baseURL', function($resource, baseURL) {
+
+
+                this.getBudgets = function () {
+                    return $resource(baseURL+"budget/:id",null,  {'update':{method:'PUT'}});
+                };
+
+
+                        
+        }])
+
+
+
+;
