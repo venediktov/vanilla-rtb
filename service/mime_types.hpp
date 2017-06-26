211fb5aa63353d5bb385be350964bea2c2b381cc
Vladimir Venediktov <vvenedict@gmail.com>
2017-06-25 20:03:25 -0700

    Squashed 'CRUD/' changes from eef5413..51c9134
    51c9134 refactor mime
    
    git-subtree-dir: CRUD
    git-subtree-split: 51c9134bd54911112ab5a51e54b42b7955357838

--- service/mime_types.hpp
+++ service/mime_types.hpp
@@ -7,6 +7,9 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+// Modified by: Vladimir Venediktov
+// Introduced constexpr for compile time use case
+//
 
 #ifndef HTTP_MIME_TYPES_HPP
 #define HTTP_MIME_TYPES_HPP
@@ -16,6 +19,51 @@
 namespace http {
 namespace server {
 namespace mime_types {
+
+struct mapping
+{
+  const char* extension;
+  const char* mime_type;
+} constexpr mappings[] =
+{
+  { "gif", "image/gif" },
+  { "htm", "text/html" },
+  { "html", "text/html" },
+  { "jpg", "image/jpeg" },
+  { "png", "image/png" },
+  { "json", "application/json"},
+  { "css", "text/css"}
+};
+
+
+constexpr bool equal( char const* lhs, char const* rhs )
+{
+    while (*lhs || *rhs)
+        if (*lhs++ != *rhs++)
+            return false;
+    return true;
+}
+
+constexpr const char* extension_to_type(const char* extension)
+{
+  for (mapping m: mappings)
+  {
+    if (equal(m.extension,extension) )
+    {
+      return m.mime_type;
+    }
+  }
+
+  return "text/plain";
+}
+
+constexpr const char* GIF  = mime_types::extension_to_type("gif");
+constexpr const char* HTM  = mime_types::extension_to_type("htm");
+constexpr const char* HTML = mime_types::extension_to_type("html");
+constexpr const char* JPG  = mime_types::extension_to_type("jpg");
+constexpr const char* PNG  = mime_types::extension_to_type("png");
+constexpr const char* JSON = mime_types::extension_to_type("json");
+constexpr const char* CSS  = mime_types::extension_to_type("css");
 
 /// Convert a file extension into a MIME type.
 std::string extension_to_type(const std::string& extension);
