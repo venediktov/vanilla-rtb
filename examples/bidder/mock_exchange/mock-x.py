b4f5c96fc8e4363d795c2cd76e8fb82d27c69edc
abushev <arseny.bushev@gmail.com>
2017-07-26 00:53:47 +0300

    mock-x.py template based requests construction

--- examples/bidder/mock_exchange/mock-x.py
+++ examples/bidder/mock_exchange/mock-x.py
@@ -13,22 +13,25 @@ import threading
 import signal
 from time import sleep
 import argparse
-import sys
+import random 
 
 class Exchange:
-    def __init__(self, limit, url, files, timeout, concurrency):
+    def __init__(self, args):
+        self.concurrency = concurrency=int(args.concurrency)
         self.exchange = True 
-        self.limit = limit/concurrency
-        self.threads = []
-        self.concurrency = concurrency
+        self.limit = int(args.limit)/concurrency
+        self.threads = []     
         self.lock = threading.Lock()
         self.threads_finished = 0
         self.exchange_data  = []
-        self.url = url
-        self.timeout = timeout
-        for f in files:
-            with open(f, 'rb') as data_file:
+        self.url = args.url
+        self.timeout = float(args.timeout)
+        for f in args.requests.split(" "):
+            with open(f, 'r') as data_file:
                 self.exchange_data.append(data_file.read())  
+        self.geo = [s.split(':') for s in str(args.geo).split(' ')]
+        self.size = map(lambda s: [int(x) for x in s.split(':')], str(args.size).split())
+        
                 
     def run(self):
         for c in range(0, self.concurrency):
@@ -52,7 +55,15 @@ class Exchange:
             if self.limit > 0 and idx >= self.limit:
                 break
             try:
-                r = session.post(self.url, data=self.exchange_data[idx%len(self.exchange_data)], timeout=self.timeout)
+                geo = random.choice(self.geo)
+                size = random.choice(self.size)
+                data = str(self.exchange_data[idx%len(self.exchange_data)]) % {
+                    "country": geo[0],
+                    "city": geo[1],
+                    "width": size[0],
+                    "height": size[1]
+                }
+                r = session.post(self.url, data=data, timeout=self.timeout)
             except requests.exceptions.Timeout:
                 print('Connection timeout Timeout occured')
             except requests.exceptions.ConnectionError:
@@ -68,12 +79,14 @@ class Exchange:
 parser = argparse.ArgumentParser(description="""Mock exchange""")
 parser.add_argument('--limit', help='total requests number, 0 for endless requests (default 0)', default=0)
 parser.add_argument('--url', help='exchange url (default http://localhost:9081/bid/123)', default="http://localhost:9081/bid/123")
-parser.add_argument('--requests', help='Stored bid requests "file1 [file2] ... [fileN] (default data.json)"', default="data.json")
+parser.add_argument('--requests', help='Stored bid requests "file1 [file2] ... [fileN] (default data.json.template)"', default="data.json.template")
 parser.add_argument('--timeout', help='request timeout (default 0.1 sec)', default=0.1)
 parser.add_argument('--concurrency', help='threads to execute (default 5)', default=5)
+parser.add_argument('--geo', help='geo - country1:city1 [country2:city2] ...[countryN:cityN] (default Russia:Moscow)', default="Russia:Moscow")
+parser.add_argument('--size', help='size - widthN:heightN [widthN:heightN] ... [widthN:heightN] (default 100:300)', default="100:300")
 args = parser.parse_args()
 
-exchange = Exchange(limit=int(args.limit), files=args.requests.split(" "), url=args.url, timeout=float(args.timeout), concurrency=int(args.concurrency)) 
+exchange = Exchange(args) 
 signal.signal(signal.SIGINT, lambda s, f: exchange.stop())            
 exchange.run()
 
