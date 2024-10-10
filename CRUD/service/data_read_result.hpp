7b559b635ab0a2c0af1b0d34c2d423938f972587
cxx23 <164133010+cxx24@users.noreply.github.com>
2024-10-09 23:10:40 -0700

    Add files via upload

---
+++ CRUD/service/data_read_result.hpp
@@ -0,0 +1,11 @@
+//
+// Created by VanillaRTB on 10/9/24.
+//
+#pragma once
+#ifndef VANILLA_RTB_DATA_READ_RESULT_HPP
+#define VANILLA_RTB_DATA_READ_RESULT_HPP
+/// Result of data read.
+namespace http::server {
+enum data_read_result { complete, indeterminate };
+}
+#endif // VANILLA_RTB_DATA_READ_RESULT_HPP
