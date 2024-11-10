98d9e5dcc4e326555034698fa7455caabe59a8bf
cxx23 <164133010+cxx24@users.noreply.github.com>
2024-11-10 02:54:16 -0800

    CRUD allow custom matchers (#107)
    Co-authored-by: cxx24 <cxx24@github.com>

--- CRUD/handlers/crud_matcher.hpp
+++ CRUD/handlers/crud_matcher.hpp
@@ -4,57 +4,28 @@
 *
 * Created on September 29, 2015, 6:06 PM
 */
- 
-#ifndef _HTTP_CRUD_MATCHER_HPP__
-#define                _HTTP_CRUD_MATCHER_HPP__
- 
+#pragma once
+#ifndef HTTP_CRUD_MATCHER_HPP
+#define HTTP_CRUD_MATCHER_HPP
+
 #include <functional>
 #include <map>
 #include <string>
- 
-namespace http { namespace crud {
-    
+#include "crud_matcher_base.hpp"
+
+
+namespace http::crud {
+
     template<typename Matched>
     struct crud_match : Matched {
-        crud_match(const Matched &m, const std::string &d) : Matched(m) , data(d) {}
+        template<typename Request>
+        crud_match(const Matched &m, const Request &request) : Matched(m) , data(request.data) {}
         std::string data;
     };
-    
+
     template<typename Response, typename Regex, typename Matched>
-    struct crud_matcher {
-        typedef std::function<void(Response &, const crud_match<Matched> &)>  request_handler_type;
-        typedef crud_matcher<Response, Regex, Matched> self_type ;
-        explicit crud_matcher(const Regex &expression) : _expression(expression) {}
-        self_type & get(request_handler_type handler) {
-            _handlers["GET"] = handler;
-            return *this ;
-        }
-        self_type & post(request_handler_type handler) {
-            _handlers["POST"] = handler;
-            return *this;
-        }
-        self_type & del(request_handler_type handler) {
-            _handlers["DELETE"] = handler;
-            return *this;
-        }
-       self_type & put(request_handler_type handler) {
-            _handlers["PUT"] = handler;
-            return *this;
-        }
-        template<typename Request>
-        void handle_request(const Request& request, Response& response, const Matched &what) {
-             //dispatching to matching based on CRUD handler
-            crud_match<Matched> match(what, request.data) ;
-            auto &handler = _handlers[request.method];
-            if (handler) {
-                handler(response, match);
-            }
-        }
-    private:
-       Regex _expression;
-       std::map<std::string, request_handler_type> _handlers;
-    };
- 
-}}
- 
-#endif   /* __HTTP_CRUD_MATCHER__ */
+    struct crud_matcher :  crud_matcher_base<Response, crud_match<Matched>> {};
+
+}
+
+#endif   /* HTTP_CRUD_MATCHER_HPP */
